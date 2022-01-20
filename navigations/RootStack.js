36ee2c0a73e3aca3c1b82f677ab7f5b96ae34334
fxlive <86455610+fewxlive@users.noreply.github.com>
2022-01-20 23:10:48 +0700

    week9

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -5,6 +5,9 @@ import PokemonTab from "./PokemonTab";
 import Travel from "../screens/week3/Travel";
 import Resort from "../screens/week4/Resort";
 import Health from "../screens/week5/Health";
+import Book from "../screens/week9/Book";
+import BookDetail from "../screens/week9/BookDetail";
+import BookForm from "../screens/week9/BookForm";
 const Stack = createStackNavigator();
 
 export default function RootStack() {
@@ -15,6 +18,10 @@ export default function RootStack() {
       <Stack.Screen name="Resort" component={Resort} options={{ title : "Resort" }}  />
       <Stack.Screen name="Health" component={Health} options={{ title : "Health" }}  />
       <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
+      <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
+      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
+      <Stack.Screen name="BookForm" component={BookForm}   />
+
     </Stack.Navigator>
   );
 }
